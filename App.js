1dbe8aae4e78548f1f2b251abf6aca504735f5fa
Sheikh Mubashir <smubashir655@gmail.com>
2021-09-02 16:09:46 +0500

    Update App.js

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native';
 
 export default function App() {
+  const click = () => {console.log("Clicked")}
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={{paddingTop: 20}} onPress={click} numberOfLines={1}>Welcome to React Native expo</Text>
+      <Image source={require("./assets/icon.png")} />
+    </SafeAreaView>
   );
 }
 
@@ -15,7 +15,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
 });
