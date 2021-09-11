c7d7a6ba420e68feb7800b2febc17924008518a1
Sheikh Mubashir <smubashir655@gmail.com>
2021-09-11 17:08:18 +0500

    abc

--- App.js
+++ App.js
@@ -1,36 +1,14 @@
 import React from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  SafeAreaView,
-  Image,
-  TouchableNativeFeedback,
-  Button,
-  Alert,
-  Platform,
-  StatusBar,
-} from "react-native";
-import { useDimensions, useDeviceOrientation } from "@react-native-community/hooks";
-export default function App() {
-  console.log(useDeviceOrientation())
-  const {landscape} = useDeviceOrientation();
-  const click = () => {
-    console.log("Clicked");
-  };
-  return (
-    <SafeAreaView style={styles.container}>
-      <View
-        style={{ backgroundColor: "dodgerblue", width: "100%", height: landscape ? '100%' : "30%" }}
-      ></View>
-    </SafeAreaView>
-  );
-}
+import { View, Text } from "react-native";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-  },
-});
+const App = () => {
+  return (
+    <View style={{ flexDirection: "row", flex: 1, backgroundColor: "#fff" }}>
+      <View style={{ width: 100, height: 100, backgroundColor: "red" }} />
+      <View style={{ width: 100, height: 100, backgroundColor: "green" }} />
+      <View style={{ width: 100, height: 100, backgroundColor: "yellow" }} />
+    </View>
+  );
+};
+
+export default App;
